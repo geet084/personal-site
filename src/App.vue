36d6899b5b589da2ff730c12755831aa29a9e1f5
Travis Gee <geet084@gmail.com>
2019-07-20 14:25:41 -0600

    Adds background image and styling

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,27 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: url('assets/main-bg.jpg');
+  background-size: cover no-repeat;
+  width: 100%;
+  height: 100%;
+}
+body::after {
+  content: "";
+  background: rgba(0, 0, 0, .6);
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: -2;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
