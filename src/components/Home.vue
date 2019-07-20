d2d0c58513fcd3d6d36e4995a91313f6704c203c
Travis Gee <geet084@gmail.com>
2019-07-20 14:25:41 -0600

    Adds background image and styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="home">
     <h1>{{ msg }}</h1>
   </div>
 </template>
@@ -14,5 +14,8 @@ export default {
 </script>
 
 <style scoped>
-
+.home {
+  background-color: rgb(82, 82, 82);
+  color: white;
+}
 </style>
