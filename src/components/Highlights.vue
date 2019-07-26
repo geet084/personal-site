dec5dda02dd30db16c49412b0d6f426210f186a1
Travis Gee <geet084@gmail.com>
2019-07-26 14:08:24 -0600

    Refactors project toggle functionality and adds project description to expanded project

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,46 +1,118 @@
 <template>
   <article class="highlights-page">
-    <section class="projects">
-      <div @click="toggleProject" class="project1 project">
+    <div class="projects">
+      <section @click="toggleProject" class="project1 project">
         <h3>Turing Mentors</h3>
-      </div>
-      <div @click="toggleProject" class="project2 project">
+        <div v-if="showProject==='project1'" class="content">
+          <span>
+            <a href="https://turing-mentors.herokuapp.com/">Deployed Site</a>
+            <h4>Turing Mentors</h4>
+            <a href="https://github.com/geet084/turing-mentors">GitHub page</a>
+          </span>
+          <p>Description</p>
+        </div>
+      </section>
+
+      <section @click="toggleProject" class="project2 project">
         <h3>Palette Picker</h3>
-      </div>
-      <div @click="toggleProject" class="project3 project">
+        <div v-if="showProject==='project2'" class="content">
+          <span>
+            <a href="https://palette-picker-1810.herokuapp.com/">Deployed Site</a>
+            <h4>Palette Picker</h4>
+            <a href="https://github.com/geet084/fe-palette-picker">GitHub page</a>
+          </span>
+          <p>Description</p>
+        </div>
+      </section>
+
+      <section @click="toggleProject" class="project3 project">
         <h3>Travelr</h3>
-      </div>
-      <div @click="toggleProject" class="project4 project">
+        <div v-if="showProject==='project3'" class="content">
+          <span>
+            <p></p>
+            <h4>Palette Picker</h4>
+            <a href="https://github.com/geet084/travelr">GitHub page</a>
+          </span>
+          <p>Description</p>
+        </div>
+      </section>
+
+      <section @click="toggleProject" class="project4 project">
         <h3>Trapper Keeper</h3>
-      </div>
-    </section>
+        <div v-if="showProject==='project4'" class="content">
+          <span>
+            <p></p>
+            <h4>Palette Picker</h4>
+            <a href="https://github.com/geet084/trapper-keeper">GitHub page</a>
+          </span>
+          <p>Description</p>
+        </div>
+      </section>
+    </div>
   </article>
 </template>
 
 <script>
 export default {
   name: "Highlights",
+  data() {
+    return {
+      showProject: ""
+    };
+  },
   methods: {
-    toggleProject(e) {
-      let outer = e.target.parentElement.classList;
-      let inner = e.target.classList;
-      let innerEl = outer.contains("projects");
+    toggleProject({ target }) {
+      let selection = target.closest(".project") || target.closest(".expanded");
 
-      let target = innerEl ? inner : outer;
-
-      if (target.contains("expanded")) {
-        innerEl ? inner.remove("expanded") : outer.remove("expanded");
-        innerEl ? inner.add("project") : outer.add("project");
+      if (selection.classList.contains("expanded")) {
+        selection.classList.remove("expanded");
+        selection.classList.add("project");
       } else {
-        innerEl ? inner.add("expanded") : outer.add("expanded");
-        innerEl ? inner.remove("project") : outer.remove("project");
+        selection.classList.add("expanded");
+        selection.classList.remove("project");
       }
+      let project = selection.classList[0];
+      this.showProject = this.showProject !== project ? project : "";
     }
   }
 };
 </script>
 
 <style scoped>
+.content {
+  position: absolute;
+  top: 140px;
+  left: 17%;
+  z-index: 3;
+  background-color: rgb(255, 255, 255, 0.85);
+  color: rgb(189, 29, 29);
+  font-size: 1rem;
+  font-weight: 600;
+  width: 66%;
+  overflow: scroll;
+  height: 275px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  border: 1px solid rgb(101, 101, 101);
+}
+.content a {
+  color: rgb(189, 29, 29);
+}
+.content a:hover {
+  color: rgb(0, 64, 255);
+}
+.content span {
+  width: 100%;
+  margin: 5px 0;
+  display: flex;
+  justify-content: space-around;
+}
+.content p {
+  margin: 10px 10px 0;
+}
+
 .expanded {
   display: flex;
   margin: 40px 0;
@@ -78,18 +150,18 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
-  width: 100vw;
+  margin: 0 15%;
 }
 .project {
   display: flex;
-  margin: 40px 0;
+  margin: 25px 0;
   align-items: center;
   justify-content: center;
   border-radius: 15px;
   content: "";
   position: relative;
   width: 100%;
-  height: 100%;
+  height: 70px;
 }
 .project:hover {
   cursor: pointer;
@@ -101,7 +173,7 @@ export default {
 .project:before {
   content: "";
   position: absolute;
-  width: 70%;
+  width: 100%;
   height: 70px;
   z-index: -1;
   opacity: 0.5;
