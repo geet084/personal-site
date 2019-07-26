2c384c0a650bd53698674e2271a92a3f1d9320ec
Travis Gee <geet084@gmail.com>
2019-07-26 15:09:41 -0600

    Adds palette picker description

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -29,7 +29,15 @@
             <h4>Palette Picker</h4>
             <a href="https://github.com/geet084/fe-palette-picker">GitHub Repo</a>
           </span>
-          <p>Description</p>
+          <p>
+            My first project where we developed an entire fullstack application from start to finish. Tasked with building a backend server along with using continuous integration for our API and building a frontend site that was simple and intuitive to use.
+          </p>
+          <p>
+            For the server we used the standard CRUD endpoints as well as adding another queryable GET endpoint that would search for projects by name. We were also able to implement TDD during this portion of the project, to a high degree of success.
+          </p>
+          <p>
+            On the frontend we sought to build a user interface that was easy to understand and use while also being a stylish recreation of the website Coolors. 
+          </p>
         </div>
       </section>
 
