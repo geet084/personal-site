439b38db66134914f1dee58e04ba147a86ee4298
Travis Gee <geet084@gmail.com>
2019-07-26 15:24:02 -0600

    Adds travelr description

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -46,10 +46,18 @@
         <div v-if="showProject==='project3'" class="content">
           <span>
             <p></p>
-            <h4>Palette Picker</h4>
+            <h4>Travelr</h4>
             <a href="https://github.com/geet084/travelr">GitHub Repo</a>
           </span>
-          <p>Description</p>
+          <p>
+            For my last solo project of Turing, I wanted to build an application that would illustrate time and distance in a unique way. Many people do not realize how far they are traveling every second, due to the Earth, Sun, Solar System and Galaxy all moving through space.
+          </p>
+          <p>
+            I struggled quite a bit with Date and Time issues during the project, but was pleased to be able to get a decent (if not always accurate) representation of distance covered through space since a given date. 
+          </p>
+          <p>
+            As I continue to work on this, I would like to have a visual representation of distance pop up in correlation to the number. For example 238,000 miles and a picture of the moon. I would also like to build my own server to use as a way of storing the data and making the experience a bit better.
+          </p>
         </div>
       </section>
 
