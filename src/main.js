50359ed24b555517be9dd66cff8dd48364f5577e
Travis Gee <geet084@gmail.com>
2019-07-27 12:10:27 -0600

    Moves shared project styling into project.css file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import axios from 'axios'
+import './assets/project.css'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
