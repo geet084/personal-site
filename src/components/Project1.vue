4177f4dbe1b84c9bf5e2e82c6f81df33d45d6d64
Travis Gee <geet084@gmail.com>
2019-07-27 11:52:44 -0600

    Moves project one details into its own component

---
+++ src/components/Project1.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="content">
+    <span>
+      <a href="https://turing-mentors.herokuapp.com/">Deployed Site</a>
+      <h4>Turing Mentors</h4>
+      <a href="https://github.com/geet084/turing-mentors">GitHub Repo</a>
+    </span>
+    <p>
+      This project was my last one while at Turing and I collaborated with a backend student to build a fullstack application. We decided to build something that would enhance and replace the existing mentoring spreadsheet, which was clunky and not well looked after.
+    </p>
+    <p>
+      To do this, we came up with a short survey for both those seeking a mentor and those wished to become a mentor themselves. Using that information, we then filtered down the list by location (currently Denver or Remote) as well as by programming language to provide a better search experience.
+    </p>
+    <p>
+      Our next steps will be to implement a login and tracking system for users, more location and language selections, as well as an admin dashboard.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project1"
+};
+</script>
+
+<style scoped>
+.content {
+  position: absolute;
+  top: 140px;
+  left: 19%;
+  z-index: 3;
+  background-color: rgb(255, 255, 255, 0.85);
+  color: rgb(189, 29, 29);
+  font-size: 1rem;
+  font-weight: 600;
+  width: 62%;
+  overflow: scroll;
+  height: 300px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  border: 1px solid rgb(101, 101, 101);
+}
+.content a {
+  color: rgb(189, 29, 29);
+}
+.content a:hover {
+  color: rgb(0, 64, 255);
+}
+.content span {
+  width: 100%;
+  margin: 5px 0;
+  display: flex;
+  justify-content: space-around;
+}
+.content p {
+  margin: 10px 10px 0;
+  font-size: .85rem;
+  font-weight: 400;
+}
+</style>
