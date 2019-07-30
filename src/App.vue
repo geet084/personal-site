cb5b3b5dce992185b6bdfdd79d11dd90b941e061
Travis Gee <geet084@gmail.com>
2019-07-30 15:50:54 -0600

    Adjusts formatting

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,26 @@
   <main id="app">
     <Header />
     <Nav />
-    <router-view/>
+    <router-view />
     <Footer />
   </main>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Nav from './components/Nav.vue'
-import Home from './components/Home.vue'
-import Footer from './components/Footer.vue'
+import Header from "./components/Header.vue";
+import Nav from "./components/Nav.vue";
+import Home from "./components/Home.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
     Nav,
     Home,
     Footer
   }
-}
+};
 </script>
 
 <style>
@@ -31,14 +31,14 @@ export default {
 }
 
 body {
-  background: url('assets/main-bg.jpg');
+  background: url("assets/main-bg.jpg");
   background-size: cover no-repeat;
   width: 100%;
   height: 100%;
 }
 body::after {
   content: "";
-  background: rgba(0, 0, 0, .6);
+  background: rgba(0, 0, 0, 0.6);
   position: absolute;
   top: 0px;
   left: 0px;
@@ -47,7 +47,7 @@ body::after {
   z-index: -2;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
