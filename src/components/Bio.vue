74700eba8b39d22def78c27b0f63af7a88891fec
Travis Gee <geet084@gmail.com>
2019-07-30 13:17:10 -0600

    Adds initial bio text and styling

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,6 +1,8 @@
 <template>
-  <article>
-    <h2>BIO</h2>
+  <article class="bio-page">
+    <p>
+      Travis Gee is an adventurer and seeker of interesting things. An avid offroad explorer, he likes to take his modified Jeep into the mountains to find new ways to appreciate the beauty of nature. Some times it is hidden where few people have gone and behind very narrow dirt roads with rocks as tall as your waist 12,000 feet up on the side of a mountain. Other times it is just off the highway for everyone to see.
+    </p>
   </article>
 </template>
 
@@ -11,4 +13,18 @@ export default {
 </script>
 
 <style scoped>
+.bio-page {
+  height: 500px;
+  width: 70vw;
+  margin: auto;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  text-align: left;
+}
+.bio-page p {
+  margin: 30px 0;
+  line-height: 1.75rem;
+}
 </style>
