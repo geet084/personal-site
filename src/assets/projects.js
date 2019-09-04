14a3b3c61be557feb6ae43ade8d7d4a08994c2d9
Travis Gee <geet084@gmail.com>
2019-09-04 14:03:51 -0600

    Adds projects dataset to utilize reusable project component

---
+++ src/assets/projects.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: 'Turing Mentors',
+    site: 'https://turing-mentors.herokuapp.com/',
+    repo: 'https://github.com/geet084/turing-mentors',
+    paragraphs: [
+      'This project was my last one while at Turing and I collaborated with a backend student to build a fullstack application. We decided to build something that would enhance and replace the existing mentoring spreadsheet, which was clunky and not well looked after.',
+      'To do this, we came up with a short survey for both those seeking a mentor and those wished to become a mentor themselves. Using that information, we then filtered down the list by location (currently Denver or Remote) as well as by programming language to provide a better search experience.',
+      'Our next steps will be to implement a login and tracking system for users, more location and language selections, as well as an admin dashboard.'
+    ]
+  },
+  {
+    title: 'Palette Picker',
+    site: 'https://palette-picker-1810.herokuapp.com/',
+    repo: 'https://github.com/geet084/fe-palette-picker',
+    paragraphs: [
+      'My first project where we developed an entire fullstack application from start to finish. Tasked with building a backend server along with using continuous integration for our API and building a frontend site that was simple and intuitive to use.',
+      'For the server we used the standard CRUD endpoints as well as adding another queryable GET endpoint that would search for projects by name. We were also able to implement TDD during this portion of the project, to a high degree of success.',
+      'On the frontend we sought to build a user interface that was easy to understand and use while also being a stylish recreation of the website Coolors.',
+    ]
+  },
+  {
+    title: 'Travelr',
+    site: 'https://travelr-fe.herokuapp.com/',
+    repo: 'https://github.com/geet084/travelr',
+    paragraphs: [
+      'For my last solo project of Turing, I wanted to build an application that would illustrate time and distance in a unique way. Many people do not realize how far they are traveling every second, due to the Earth, Sun, Solar System and Galaxy all moving through space.',
+      'I struggled a lot with Date and Time issues during the project, but was very pleased when I implemented moment.js. This gives an accurate representation of distance covered through space since a given date.',
+      'As I continue to work on this, I would like to have a visual representation of distance pop up in correlation to the number. For example 238,000 miles and a picture of the moon.',
+    ]
+  },
+  {
+    title: 'Trapper Keeper',
+    site: '',
+    repo: 'https://github.com/geet084/trapper-keeper',
+    paragraphs: [
+      'There are many things that Google does right and for this project we were tasked with recreating some of that functionality. Being able to create a note along with items on each note, but to do so dynamically as needed.',
+      'It was our first foray into building a backend server to store the data that would come from out frontend. It was also the first time that we were able to successfully implement TDD from start to finish on the server functionality.',
+      'On the frontend, we sought to make the interface as simple and "Google-like" as possible and found it to be challenging at times. Simple and intuitive is not an easy experience to implement, but we felt like we ended up in a really good spot in the end.',
+    ]
+  }
+]
