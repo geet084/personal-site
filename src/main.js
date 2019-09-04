6ea9d1f0d572fb876f60ff10cd5fb8422b6f6cd7
Travis Gee <geet084@gmail.com>
2019-09-04 14:01:55 -0600

    Removes static project components & styling sheet

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import axios from 'axios'
-import './assets/project.css'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
