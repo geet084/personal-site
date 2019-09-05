7db7a4f80787b8878f0b93b3fb7147f9a4fbd621
Travis Gee <geet084@gmail.com>
2019-09-04 19:23:28 -0600

    Adds styling & scroll arrows to Highlights for shorter mobile screens

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,5 +1,6 @@
 <template>
-  <article class="highlights-page">
+  <article class="highlights-page" v-on:scroll="handleScrollArrows">
+    <span class="arrow top-arrow hide">▲</span>
     <div class="projects">
       <section
         @click="toggleProject"
@@ -17,6 +18,7 @@
         />
       </section>
     </div>
+    <span class="arrow btm-arrow hide">▼</span>
   </article>
 </template>
 
@@ -35,6 +37,15 @@ export default {
       showProject: ""
     };
   },
+  mounted() {
+    this.handleScrollArrows();
+  },
+  created() {
+    window.addEventListener("resize", this.handleScrollArrows);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleScrollArrows);
+  },
   methods: {
     toggleProject({ target }) {
       let selection = target.closest(".project") || target.closest(".expanded");
@@ -48,6 +59,21 @@ export default {
       }
       let project = selection.classList[0];
       this.showProject = this.showProject !== project ? project : "";
+    },
+    handleScrollArrows() {
+      const content = window.document.querySelector(".highlights-page");
+      const downArrow = document.querySelector("article.highlights-page .btm-arrow");
+      const upArrow = document.querySelector("article.highlights-page .top-arrow");
+      const distToBtm = content.scrollHeight - content.offsetHeight - content.scrollTop;
+      const isScrollable = content.scrollHeight > 0;
+      
+      if (content.scrollTop > 22) upArrow.classList.remove("hidden");
+      else upArrow.classList.add("hidden");
+
+      if (isScrollable && distToBtm > 5)
+        downArrow.classList.remove("hide", "hidden");
+      else if (distToBtm <= 5) downArrow.classList.add("hidden");
+      else downArrow.classList.add("hide");
     }
   }
 };
@@ -144,7 +170,37 @@ export default {
   text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
     1px 1px 0 #000;
 }
-
+.hide {
+  display: none;
+}
+.hidden {
+  visibility: hidden;
+}
+.projects span {
+  width: 300px;
+}
+.arrow {
+  color: rgb(189, 29, 29);
+  text-align: center;
+  margin: 0px;
+}
+.btm-arrow {
+  box-shadow: inset 0px -20px 10px 3px rgb(13, 13, 13);
+  position: sticky;
+  content: "";
+  display: inline-block;
+  width: 305px;
+  bottom: 0px;
+}
+.top-arrow {
+  box-shadow: inset 0px 20px 5px 0px rgb(13, 13, 13);
+  position: sticky;
+  display: inline-block;
+  content: "";
+  width: 305px;
+  top: 0px;
+  z-index: 1;
+}
 @media screen and (max-width: 462px) {
   .project,
   .expanded::before {
@@ -156,6 +212,17 @@ export default {
     position: fixed;
     top: 309px;
     width: 286px;
+  }
+}
+@media screen and (max-height: 600px) {
+  .highlights-page {
+    height: 60vh;
+  }
+  .expanded::before {
+    height: 355px;
+  }
+  .content {
+    height: 225px;
   }
 }
 </style>
